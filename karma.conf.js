a433dc1baf44c1da0e0dbd3c1e877656ce0b75bb
Nicolas COURET <nicolas.couret@gmail.com>
2021-04-13 08:50:13 +0200

    change sapui5 version + proxy

--- karma.conf.js
+++ karma.conf.js
@@ -1,30 +1,44 @@
-module.exports = function(config) {
+module.exports = function (config) {
 	"use strict";
 
-	var chromeFlags = [
-		"--window-size=1280,1024"
-	];
+	var chromeFlags = ["--window-size=1280,1024"];
 
 	config.set({
-
 		frameworks: ["ui5"],
+
+		ui5: {
+			url: "https://sapui5.hana.ondemand.com",
+			config: {
+				async: true,
+				resourceRoots: {
+					"sap.ui.demo.todo": "./webapp",
+				},
+			},
+		},
+
+		proxies: {
+			"/base/webapp": {
+				target: "https://sapui5.hana.ondemand.com/1.71.35",
+				changeOrigin: true,
+			},
+		},
+		logLevel: config.LOG_DEBUG,
 
 		browsers: ["CustomChrome"],
 
 		browserConsoleLogOptions: {
-			level: "error"
+			level: "error",
 		},
 
 		customLaunchers: {
 			CustomChrome: {
 				base: "Chrome",
-				flags: chromeFlags
+				flags: chromeFlags,
 			},
 			CustomChromeHeadless: {
 				base: "ChromeHeadless",
-				flags: chromeFlags
-			}
+				flags: chromeFlags,
+			},
 		},
-
 	});
 };
